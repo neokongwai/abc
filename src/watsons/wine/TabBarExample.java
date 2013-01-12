96e1d311334170dc6b55e82ded60a8d752ce37f9
neokongwai <neo.csc3210@gmail.com>
2013-01-12 15:31:37 +0800

    add wine list tab

--- src/watsons/wine/TabBarExample.java
+++ src/watsons/wine/TabBarExample.java
@@ -1,12 +1,23 @@
 package watsons.wine;
 
 import android.app.TabActivity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TabHost;
 import android.widget.TabHost.TabSpec;
 
 public class TabBarExample extends TabActivity {
+
+	private TabHost mTabHost;
+	
+	private void setupTabHost() {
+		mTabHost = (TabHost) findViewById(android.R.id.tabhost);		
+		mTabHost.setup();
+	}
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -14,8 +25,10 @@ public class TabBarExample extends TabActivity {
 		setContentView(R.layout.tab);
 
 		/** TabHost will have Tabs */
-		TabHost tabHost = (TabHost) findViewById(android.R.id.tabhost);
-
+		setupTabHost();
+		/*TabHost tabHost = (TabHost) findViewById(android.R.id.tabhost);
+		tabHost.getTabWidget().setStripEnabled(false);
+		tabHost.getTabWidget().setDividerDrawable(null);*/
 		/**
 		 * TabSpec used to create a new tab. By using TabSpec only we can able
 		 * to setContent to the tab. By using TabSpec setIndicator() we can set
@@ -23,31 +36,53 @@ public class TabBarExample extends TabActivity {
 		 */
 
 		/** tid1 is firstTabSpec Id. Its used to access outside. */
-		TabSpec firstTabSpec = tabHost.newTabSpec("tid1");
-		TabSpec secondTabSpec = tabHost.newTabSpec("tid2");
-		TabSpec thirdTabSpec = tabHost.newTabSpec("tid3");
-		TabSpec fourthTabSpec = tabHost.newTabSpec("tid4");
-		TabSpec fifthTabSpec = tabHost.newTabSpec("tid5");
+/*		TabSpec firstTabSpec = mTabHost.newTabSpec("tid1");
+		TabSpec secondTabSpec = mTabHost.newTabSpec("tid2");
+		TabSpec thirdTabSpec = mTabHost.newTabSpec("tid3");
+		TabSpec fourthTabSpec = mTabHost.newTabSpec("tid4");
+		TabSpec fifthTabSpec = mTabHost.newTabSpec("tid5");*/
 
 		/** TabSpec setIndicator() is used to set name for the tab. */
 		/** TabSpec setContent() is used to set content for a particular tab. */
-		firstTabSpec.setIndicator("First Tab Name").setContent(
-				new Intent(this, FirstTab.class));
-		secondTabSpec.setIndicator("Second Tab Name").setContent(
+		setupTab(new ImageView(this),R.drawable.ic_tab_winelist,WineListTab.class);
+		setupTab(new ImageView(this),R.drawable.ic_tab_cellar,SecondTab.class);
+		setupTab(new ImageView(this),R.drawable.ic_tab_food,ThirdTab.class);
+		setupTab(new ImageView(this),R.drawable.ic_tab_events,FourthTab.class);
+		setupTab(new ImageView(this),R.drawable.ic_tab_location,FifthTab.class);
+		/*//firstTabSpec.setIndicator("", this.getResources().getDrawable(R.drawable.ic_tab_winelist)).setContent(
+		//		new Intent(this, FirstTab.class));
+		secondTabSpec.setIndicator("", this.getResources().getDrawable(R.drawable.ic_tab_cellar)).setContent(
 				new Intent(this, SecondTab.class));
-		thirdTabSpec.setIndicator("Third Tab Name").setContent(
+		thirdTabSpec.setIndicator("", this.getResources().getDrawable(R.drawable.ic_tab_food)).setContent(
 				new Intent(this, ThirdTab.class));
-		fourthTabSpec.setIndicator("Fourth Tab Name").setContent(
+		fourthTabSpec.setIndicator("", this.getResources().getDrawable(R.drawable.ic_tab_events)).setContent(
 				new Intent(this, FourthTab.class));
-		fifthTabSpec.setIndicator("Fifth Tab Name").setContent(
+		fifthTabSpec.setIndicator("", this.getResources().getDrawable(R.drawable.ic_tab_location)).setContent(
 				new Intent(this, FifthTab.class));
-
+*/
 		/** Add tabSpec to the TabHost to display. */
-		tabHost.addTab(firstTabSpec);
-		tabHost.addTab(secondTabSpec);
-		tabHost.addTab(thirdTabSpec);
-		tabHost.addTab(fourthTabSpec);
-		tabHost.addTab(fifthTabSpec);
+		//mTabHost.addTab(firstTabSpec);
+/*		mTabHost.addTab(secondTabSpec);
+		mTabHost.addTab(thirdTabSpec);
+		mTabHost.addTab(fourthTabSpec);
+		mTabHost.addTab(fifthTabSpec);*/
 
+	}
+	private void setupTab(final View view, final int drawable, Class<?> tab_class) {
+		View tabview = createTabView(mTabHost.getContext(), drawable);
+
+		TabSpec setContent = mTabHost.newTabSpec("test").setIndicator(tabview).setContent(
+			new Intent(this,tab_class)
+				//new TabContentFactory() {
+			//public View createTabContent(String tag) {return view;}}
+		);
+		mTabHost.addTab(setContent);
+	}
+
+	private static View createTabView(final Context context, final int drawable) {
+		View view = LayoutInflater.from(context).inflate(R.layout.tabs_bg, null);
+		ImageView iv = (ImageView) view.findViewById(R.id.tabsImage);
+		iv.setImageResource(drawable);
+		return view;
 	}
 }
---
+++ src/watsons/wine/WineListTab.java
@@ -0,0 +1,36 @@
+package watsons.wine;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class WineListTab extends Activity {
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		
+		setContentView(R.layout.winelist_tab);
+		/* Wine List Tab Content */
+		ListView listView = (ListView) findViewById(R.id.list_wine);
+		String[] values = new String[] { "Android", "iPhone", "WindowsMobile",
+		  "Blackberry", "WebOS", "Ubuntu", "Windows7", "Max OS X",
+		  "Linux", "OS/2" };
+
+		// Define a new Adapter
+		// First parameter - Context
+		// Second parameter - Layout for the row
+		// Third parameter - ID of the TextView to which the data is written
+		// Forth - the Array of data
+		
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+		  R.layout.list_wine_item, R.id.list_wineitem_text, values);
+
+		// Assign adapter to ListView
+		listView.setAdapter(adapter);
+		listView.setDividerHeight(0);
+
+	}
+}

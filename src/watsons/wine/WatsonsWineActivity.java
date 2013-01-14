7c6cbf92a8ca0f41cb9fe3e3f2378876965fae18
neokongwai <neo.csc3210@gmail.com>
2013-01-14 23:42:55 +0800

    add calendar pic

--- src/watsons/wine/WatsonsWineActivity.java
+++ src/watsons/wine/WatsonsWineActivity.java
@@ -1,19 +1,13 @@
 package watsons.wine;
 
-import com.google.android.maps.GeoPoint;
-
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
-import android.view.WindowManager;
 import android.widget.ImageView;
 
 public class WatsonsWineActivity extends Activity {
-	private static final int INITIAL_LATITUDE = 22420487;
-	private static final int INITIAL_LONGITUDE = 114207216;
-	private static final GeoPoint Central = new GeoPoint(INITIAL_LATITUDE, INITIAL_LONGITUDE);
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {

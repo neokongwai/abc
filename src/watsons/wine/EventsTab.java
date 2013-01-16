ae525ac39a9a98b41c226e5f961287b2904f93cd
neokongwai <neo.csc3210@gmail.com>
2013-01-17 00:16:41 +0800

    Add Calendar View

--- src/watsons/wine/EventsTab.java
+++ src/watsons/wine/EventsTab.java
@@ -2,15 +2,46 @@ package watsons.wine;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.text.format.DateUtils;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class EventsTab extends Activity {
-	/** Called when the activity is first created. */
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
+public class EventsTab extends Activity implements CalendarView.OnCellTouchListener{
+	public static final String MIME_TYPE = "vnd.android.cursor.dir/vnd.exina.android.calendar.date";
+	CalendarView mView = null;
+	TextView mHit;
+	Handler mHandler = new Handler();
+	
+	
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.events_tab);
+        mView = (CalendarView)findViewById(R.id.calendar_view);
+        mView.setOnCellTouchListener(this); 
+    }
 
-		/* Event Tab Content */
-		setContentView(R.layout.events_tab);
+	public void onTouch(Cell cell) {
+		
+		int year  = mView.getYear();
+		int month = mView.getMonth();
+		int day   = cell.getDayOfMonth();
+		day = cell.getDayOfMonth();
+		if(mView.firstDay(day))
+			mView.previousMonth();
+		else if(mView.lastDay(day))
+			mView.nextMonth();
+		else
+			return;
 
+		mHandler.post(new Runnable() {
+			public void run() {
+				Toast.makeText(EventsTab.this, DateUtils.getMonthString(mView.getMonth(), DateUtils.LENGTH_LONG) + " "+mView.getYear(), Toast.LENGTH_SHORT).show();
+			}
+		});
 	}
+
+    
 }

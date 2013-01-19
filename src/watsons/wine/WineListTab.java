5b1b1bae7da3038adbe29076d0cabb5a99d2b3a5
neokongwai <neo.csc3210@gmail.com>
2013-01-20 02:57:02 +0800

    - Add Json Parser - Add Expandable List View

--- src/watsons/wine/WineListTab.java
+++ src/watsons/wine/WineListTab.java
@@ -3,32 +3,43 @@ package watsons.wine;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
-import android.widget.ListAdapter;
-import android.widget.ListView;
-import android.widget.SimpleAdapter;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ExpandableListAdapter;
+import android.widget.ExpandableListView;
+import android.widget.ImageView;
+import android.widget.SimpleExpandableListAdapter;
+import android.widget.TextView;
 
 public class WineListTab extends Activity {
 	// url to make request
     private static String url = "http://watsonwine.bull-b.com/CodeIgniter_2.1.3/index.php/api/list_countries";
+    
  
     // JSON Node names
     private static final String TAG_COUNTRIES = "countries";
     private static final String TAG_ID = "id";
     private static final String TAG_NAME = "name";
-    private static final String TAG_PROVINCE = "province";
+    private static final String TAG_PROVINCE = "provinces";
     private static final String TAG_PRODUCT_COUNT = "product_count";
  
     // contacts JSONArray
     JSONArray contries = null;
-    
+    JSONArray provinces = null;
+    JSONArray provinces_children = null;
+    private ExpandableListAdapter mAdapter;
+    Context mContext = WineListTab.this;    
     
 	/** Called when the activity is first created. */
 	@Override
@@ -38,21 +49,20 @@ public class WineListTab extends Activity {
 		setContentView(R.layout.winelist_tab);
 		/* Wine List Tab Content */
 		
-		 // Hashmap for ListView
-        ArrayList<HashMap<String, String>> contryList = new ArrayList<HashMap<String, String>>();
+		// Hashmap for ListView
+		final List<Map<String, String>> contryList = new ArrayList<Map<String, String>>();
+		List<List<Map<String, String>>> provinceList = new ArrayList<List<Map<String, String>>>();
  
         // Creating JSON Parser instance
         JSONParser jParser = new JSONParser();
  
         // getting JSON string from URL
         JSONObject json = jParser.getJSONFromUrl(url);
-        
-        
-        
+                
         try {
             // Getting Array of Contacts
         	contries = json.getJSONArray(TAG_COUNTRIES);
- 
+
             // looping through All Contacts
             for(int i = 0; i < contries.length(); i++){
                 JSONObject c = contries.getJSONObject(i);
@@ -60,22 +70,40 @@ public class WineListTab extends Activity {
                 // Storing each json item in variable
                 String id = c.getString(TAG_ID);
                 String name = c.getString(TAG_NAME);
-                String province = c.getString(TAG_PROVINCE);
+                //String province = c.getString(TAG_PROVINCE);
                 String product_count = c.getString(TAG_PRODUCT_COUNT);
- 
-                /* Phone number is agin JSON Object
-                JSONObject phone = c.getJSONObject(TAG_PHONE);
-                String mobile = phone.getString(TAG_PHONE_MOBILE);
-                String home = phone.getString(TAG_PHONE_HOME);
-                String office = phone.getString(TAG_PHONE_OFFICE);*/
- 
+                
+                // Provinces is again a JSON Object
+                try
+                {
+                	provinces = c.getJSONArray(TAG_PROVINCE);
+                }
+                catch (Exception e) {
+                    Log.e("Json Error", "province converting result " + e.toString());       
+                }
+
+                List<Map<String, String>> children = new ArrayList<Map<String, String>>();
+                for(int j = 0; j < provinces.length(); j++){
+                	JSONObject p = provinces.getJSONObject(j);
+	                String province_id = p.getString(TAG_ID);
+	                String province_name = p.getString(TAG_NAME);
+	                String province_product_count = p.getString(TAG_PRODUCT_COUNT);
+	                
+	                // creating new HashMap
+	                HashMap<String, String> p_map = new HashMap<String, String>();
+	                p_map.put(TAG_ID, province_id);
+	                p_map.put(TAG_NAME, province_name);
+	                p_map.put(TAG_PRODUCT_COUNT, province_product_count);
+	                children.add(p_map);
+                }
+                provinceList.add(children);
+                
                 // creating new HashMap
                 HashMap<String, String> map = new HashMap<String, String>();
- 
                 // adding each child node to HashMap key => value
                 map.put(TAG_ID, id);
                 map.put(TAG_NAME, name);
-                map.put(TAG_PROVINCE, province);
+                //map.put(TAG_PROVINCE, province);
                 map.put(TAG_PRODUCT_COUNT, product_count);
  
                 // adding HashList to ArrayList
@@ -84,26 +112,83 @@ public class WineListTab extends Activity {
         } catch (JSONException e) {
             e.printStackTrace();
         }
-        int len = contries.length();
-        List<String> zoom = new ArrayList<String>();
-        for (int i = 0 ; i<len ; i++)
+        
+        /*
+        List<String> contryArr = new ArrayList<String>();
+        List<String> provinceArr = new ArrayList<String>();
+        for (int i = 0 ; i<contryList.size(); i++)
         {
-        	zoom.add(contryList.get(i).get(TAG_NAME));
+        	contryArr.add(contryList.get(i).get(TAG_NAME));
         	
         }
+        for (int i = 0 ; i<provinceList.size() ; i++)
+        {
+        	//provinceArr.add(provinceList.get(i).get(TAG_NAME));
+        }*/
 
 		// Define a new Adapter
 		// First parameter - Context
 		// Second parameter - Layout for the row
 		// Third parameter - ID of the TextView to which the data is written
 		// Forth - the Array of data
-		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-		  R.layout.list_wine_item, R.id.list_wineitem_text, zoom);
+		//ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+		//  R.layout.list_wine_item, R.id.list_wineitem_text, contryArr);
+
+		 mAdapter = new SimpleExpandableListAdapter(
+	                this,
+	                contryList,
+	                R.layout.list_wine_item,
+	                new String[] {TAG_NAME},
+	                new int[] { R.id.list_wineitem_text },
+	                provinceList,
+	                R.layout.list_wine_item,
+	                new String[] {TAG_NAME},
+	                new int[] {	R.id.list_wineitem_text }
+	                )
+		 {
+			@Override
+			public View getGroupView (int groupPosition, 
+						boolean isExpanded, 
+						View convertView, 
+						ViewGroup parent) 
+			{
+					//final View v = super.getGroupView( groupPosition, isExpanded, convertView, parent);
+					//convertView = newGroupView(isExpanded, parent);
+					LayoutInflater li = (LayoutInflater)mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+					View view = li.inflate(R.layout.list_wine_item, null);
+					ImageView inidicatorImage = (ImageView) view.findViewById(R.id.explist_indicator);
+					
+					if ( getChildrenCount( groupPosition ) == 0 ) 
+					{
+						inidicatorImage .setVisibility( View.INVISIBLE	 );
+					}
+					else
+					{	
+						inidicatorImage.setImageResource(isExpanded?R.drawable.arrow_down:R.drawable.arrow_right);
+						inidicatorImage .setVisibility( View.VISIBLE );
+					}
+					TextView tv = (TextView) view.findViewById(R.id.list_wineitem_text);
+		            tv.setText(contryList.get(groupPosition).get(TAG_NAME));
+					return view;
+			}
+			 
+		 };
 
 		// Assign adapter to ListView
-		ListView listView = (ListView) findViewById(R.id.list_wine);
-		listView.setAdapter(adapter);
+		ExpandableListView listView = (ExpandableListView) findViewById(R.id.list_wine);
+		//int width = getWindowManager().getDefaultDisplay().getWidth();
+		//listView.setIndicatorBounds(width-GetDipsFromPixel(16), width-GetDipsFromPixel(5));
+		listView.setAdapter(mAdapter);
 		listView.setDividerHeight(2);
 
 	}
+	
+	//Convert pixel to dip 
+	public int GetDipsFromPixel(float pixels)
+	{
+	        // Get the screen's density scale
+	        final float scale = getResources().getDisplayMetrics().density;
+	        // Convert the dps to pixels, based on density scale
+	        return (int) (pixels * scale + 0.5f);
+	} 
 }

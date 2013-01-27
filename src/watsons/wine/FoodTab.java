1b85896853ae87b937f48eb355e96606c89fd898
Vincent KWOK <manchunman0720@gmail.com>
2013-01-27 12:25:11 +0800

    api change food and wine top image

--- src/watsons/wine/FoodTab.java
+++ src/watsons/wine/FoodTab.java
@@ -1,7 +1,17 @@
 package watsons.wine;
 
+import java.io.InputStream;
+import java.net.URL;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import android.app.Activity;
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,14 +25,32 @@ public class FoodTab extends Activity {
 
 	int[] icons;
 	String[] names;
-
+	
+	// url to make request
+	private static String url = "http://watsonwine.bull-b.com/CodeIgniter_2.1.3/index.php/api/food_and_wine_top_image";
+	 // JSON Node names
+    private static final String TAG_CONTENT = "top_image_url";
+    private static final String TAG_URL = "url";
+	
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-
+		
 		/* Food & Wine Tab Content */
 		setContentView(R.layout.food_tab);
+		
+		/* change food and wine top image*/
+		ImageView imageView_listwine = (ImageView) findViewById(R.id.list_wine);
+		String top_image_url = get_top_img_url();
+		System.out.println(top_image_url);
+		if(top_image_url != null) {
+			Drawable d = LoadImageFromWebOperations(top_image_url);
+			Bitmap bitmap = ((BitmapDrawable) d).getBitmap();
+			Drawable dx = new BitmapDrawable(getResources(), Bitmap.createScaledBitmap(bitmap, (int)(320*480/320), (int)(220*480/320), true));
+			
+			imageView_listwine.setImageDrawable(dx);
+		}
 
 		ListView listView = (ListView) findViewById(R.id.list_food);
 		icons = new int[] { 
@@ -70,5 +98,34 @@ public class FoodTab extends Activity {
 
 			return rowview;
 		}
+	}
+	
+	public static Drawable LoadImageFromWebOperations(String url) {
+	    try {
+	        InputStream is = (InputStream) new URL(url).getContent();
+	        Drawable d = Drawable.createFromStream(is, "src name");
+	        return d;
+	    } catch (Exception e) {
+	        return null;
+	    }
+	}
+	
+	private String get_top_img_url() {
+		String return_url = null;
+		
+		// Creating JSON Parser instance
+        JSONParser jParser = new JSONParser();
+        JSONObject json = jParser.getJSONFromUrl(url);
+        JSONArray top_image_url = null;
+        try {
+            // Getting Array of Contacts
+            top_image_url = json.getJSONArray(TAG_CONTENT);
+            JSONObject c = top_image_url.getJSONObject(0);
+            return_url = c.getString(TAG_URL);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        
+        return return_url;
 	}
 }

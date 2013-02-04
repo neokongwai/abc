43b7c21d254d91a30b38d88f31731d9a6d50e0e8
Vincent KWOK <manchunman0720@gmail.com>
2013-02-04 08:05:49 +0800

    send email done

--- src/watsons/wine/WebAppInterface.java
+++ src/watsons/wine/WebAppInterface.java
@@ -1,10 +1,12 @@
 package watsons.wine;
 
+import watsons.wine.utilities.WatsonWineDB;
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.widget.Toast;
-import watsons.wine.utilities.*;
 
-public class WebAppInterface {
+public class WebAppInterface extends Activity {
     Context mContext;
 
     /** Instantiate the interface and set the context */
@@ -17,6 +19,40 @@ public class WebAppInterface {
     public void showToast(String toast) {
         Toast.makeText(mContext, toast, Toast.LENGTH_SHORT).show();
         
+    }
+    
+    public void sendmail(String wineName, String prd_id, String tasting_note) {
+    	String message1 = "I've added " + wineName + "into my wish list. Let's have a wine gathering and try together.";
+    	String message2 = "";
+    	String message3 = "";
+    	
+    	if(!prd_id.isEmpty()) {
+    		message2 = "\n" + "https://www.watsonswine.com/WebShop/BrowseProductDetail.do?prdid=" + prd_id;
+    	}
+    	if(!tasting_note.isEmpty()) {
+    		message3 = "\n" + "Tasting note: " + tasting_note;
+    	}
+    	
+    	final String message = message1 + message2 + message3;
+    	final String subject = "Share wine to you";
+    	
+    	System.out.println("msg"+message);
+    	
+    	Runnable runnable = new Runnable() {
+    		public void run() {
+    			//send email operation
+    	    	Intent email = new Intent(Intent.ACTION_SEND);
+    	    	//email.putExtra(Intent.EXTRA_CC, new String[]{ to});
+    	    	//email.putExtra(Intent.EXTRA_BCC, new String[]{to});
+    	    	email.putExtra(Intent.EXTRA_SUBJECT, subject);
+    	    	email.putExtra(Intent.EXTRA_TEXT, message);
+    	    	//need this to prompts email client only
+    	    	email.setType("message/rfc822");
+    	    	mContext.startActivity(email);
+    		}
+    	};
+    	
+    	runOnUiThread(runnable);
     }
     
     public void addToCeller(String wineName, String region, String vintage, String grape, String colour, String body, String sweetness, String size, double price, String note, String wineImage) {
